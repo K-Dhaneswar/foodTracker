5021bae0d6f13611f98af46d4bd6a796972c0508
Dhaneswar <dhaneswarkatteboyina@gmail.com>
2025-12-04 13:30:17 +0530

    ntg

--- foodTracker.js
+++ foodTracker.js
@@ -10,15 +10,13 @@ if ( !localStorage.getItem("arr") && !localStorage.getItem("lastday")) {
 }
 
 function updateValues(){
-    if(localStorage.getItem("newupdate1")){
         let foodData = JSON.parse(localStorage.getItem("foodData")) || [];
         $.each(foodData,function(index,value){
             let parts=value.date.split("/");
             value.date=parts[1]+"/"+parts[0]+"/"+parts[2];
         });
         localStorage.setItem("foodData",JSON.stringify(foodData));
-        localStorage.setItem("newupdate1","true");
-    }
+        alert(localStorage.length);
 }
 updateValues();
 
